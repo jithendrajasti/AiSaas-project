6d706d3b0810ecbd44e05982f1707dad9a969970
jithendrajasti <jithendrajasti11@gmail.com>
2025-07-26 12:17:59 +0530

    cloudinary commit

--- backend/config/cloudinaryConnect.js
+++ backend/config/cloudinaryConnect.js
@@ -1,12 +1,12 @@
-const {v2:cloudinary}=require('cloudinary');
+const { v2: cloudinary } = require('cloudinary');
 require('dotenv').config();
 
-const cloudinaryConnect=async()=>{
+const cloudinaryConnect = async () => {
     cloudinary.config({
-        cloud_name:process.env.CLOUDINARY_NAME,
-        api_key:process.env.CLOUDINARY_API_KEY,
-        api_secret:process.env.CLOUDINARY_SECRET_KEY
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
     });
 }
 
-module.exports=cloudinaryConnect;
+module.exports = cloudinaryConnect;
