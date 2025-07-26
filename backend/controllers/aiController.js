5c578c6e114f7c1a7e55c484d32c978c769a27fb
jithendrajasti <jithendrajasti11@gmail.com>
2025-07-26 13:47:11 +0530

    background issue commit

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,10 +1,10 @@
 const OpenAI = require('openai');
-const {clerkClient}=require('@clerk/express');
+const { clerkClient } = require('@clerk/express');
 const Creation = require('../models/creations');
 const { default: axios } = require('axios');
-const cloudinary=require('cloudinary');
-const pdf=require('pdf-parse/lib/pdf-parse.js');
-const fs=require('fs');
+const cloudinary = require('cloudinary');
+const pdf = require('pdf-parse/lib/pdf-parse.js');
+const fs = require('fs');
 require('dotenv').config();
 
 const ai = new OpenAI({
@@ -31,34 +31,34 @@ const generateArticle = async (req, res) => {
             messages: [
                 {
                     role: "user",
-                    content:prompt,
+                    content: prompt,
                 },
             ],
-            temperature:0.7,
-            max_tokens:length
+            temperature: 0.7,
+            max_tokens: length
         });
 
 
-        const content=response.choices[0].message.content;
+        const content = response.choices[0].message.content;
 
         //store the article generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:prompt,
-            content:content,
-            type:'article'
-        }); 
+            user_id: userId,
+            prompt: prompt,
+            content: content,
+            type: 'article'
+        });
 
-        if(plan!=='premium'){
-            await clerkClient.users.updateUserMetadata(userId,{
-                privateMetadata:{
-                    free_usage:free_usage+1
+        if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: free_usage + 1
                 }
             });
         }
 
-        res.json({success:true,content});
+        res.json({ success: true, content });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -68,7 +68,7 @@ const generateArticle = async (req, res) => {
 const generateBlogTitle = async (req, res) => {
     try {
         const { userId } = req.auth();
-        const { prompt} = req.body;
+        const { prompt } = req.body;
         const plan = req.plan;
         const free_usage = req.free_usage;
         if (plan !== 'premium' && free_usage >= 10) {
@@ -84,34 +84,34 @@ const generateBlogTitle = async (req, res) => {
             messages: [
                 {
                     role: "user",
-                    content:prompt,
+                    content: prompt,
                 },
             ],
-            temperature:0.7,
-            max_tokens:250
+            temperature: 0.7,
+            max_tokens: 250
         });
 
 
-        const content=response.choices[0].message.content;
+        const content = response.choices[0].message.content;
 
         //store the title generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:prompt,
-            content:content,
-            type:'blog-title'
-        }); 
+            user_id: userId,
+            prompt: prompt,
+            content: content,
+            type: 'blog-title'
+        });
 
-        if(plan!=='premium'){
-            await clerkClient.users.updateUserMetadata(userId,{
-                privateMetadata:{
-                    free_usage:free_usage+1
+        if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: free_usage + 1
                 }
             });
         }
 
-        res.json({success:true,content});
+        res.json({ success: true, content });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -130,36 +130,36 @@ const generateImage = async (req, res) => {
 
         //to generate image,we have to use clipDrop api
 
-       const form=new FormData();
-       form.append('prompt',prompt);
+        const form = new FormData();
+        form.append('prompt', prompt);
 
-      const {data}=await axios.post('https://clipdrop-api.co/text-to-image/v1',form,{
-        headers:{
-            'x-api-key':process.env.CLIPDROP_API_KEY
-        },
-         responseType:"arraybuffer"
-       })
+        const { data } = await axios.post('https://clipdrop-api.co/text-to-image/v1', form, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API_KEY
+            },
+            responseType: "arraybuffer"
+        })
 
-       const base64Image=`data:image/png;base64,${Buffer.from(data,'binary').toString('base64')}`;
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
 
-       //uploading image to cloud
+        //uploading image to cloud
 
-       const {secure_url}=await cloudinary.uploader.upload(base64Image);
+        const { secure_url } = await cloudinary.uploader.upload(base64Image);
 
-       const imageUrl=secure_url;
+        const imageUrl = secure_url;
 
         //store the image generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:prompt,
-            content:imageUrl,
-            type:'image',
-            publish:publish
-        }); 
+            user_id: userId,
+            prompt: prompt,
+            content: imageUrl,
+            type: 'image',
+            publish: publish
+        });
 
 
-        res.json({success:true,content:imageUrl});
+        res.json({ success: true, content: imageUrl });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -178,29 +178,31 @@ const removeBackground = async (req, res) => {
         }
 
         //to remove image background,we will do it using cloudinary
+       
+        const { secure_url } = await cloudinary.uploader.upload(image.path, {
+            transformation: [
+                {
+                    effect: 'background_removal',
+                    background_removal: 'remove_the_background'
+                }
+            ]
+        });
+        
+        const imageUrl = secure_url;
 
-       const {secure_url}=await cloudinary.uploader.upload(image.path,{
-        transformation:[
-            {
-                effect:'background_removal',
-                background_removal:'remove_the_background'
-            }
-        ]
-       });
-
-       const imageUrl=secure_url;
+        
 
         //store the image generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:'Remove background from image',
-            content:imageUrl,
-            type:'image'
-        }); 
+            user_id: userId,
+            prompt: 'Remove background from image',
+            content: imageUrl,
+            type: 'image'
+        });
 
 
-        res.json({success:true,content:imageUrl});
+        res.json({ success: true, content: imageUrl });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -209,7 +211,7 @@ const removeBackground = async (req, res) => {
 const removeObject = async (req, res) => {
     try {
         const { userId } = req.auth();
-        const {object}=req.body;
+        const { object } = req.body;
         const { image } = req.file;
         const plan = req.plan;
         if (plan !== 'premium') {
@@ -220,26 +222,26 @@ const removeObject = async (req, res) => {
 
         //to remove object,we will do it using cloudinary
 
-       const {public_id}=await cloudinary.uploader.upload(image.path);
-
-       const imageUrl=cloudinary.url(public_id,{
-        transformation:[{
-            effect:`gen_remove:${object}`
-        }],
-        resource_type:'image'
-       })
-
+        const { public_id } = await cloudinary.uploader.upload(image.path);
+        
+        const imageUrl = cloudinary.url(public_id, {
+            transformation: [{
+                effect: `gen_remove:${object}`
+            }],
+            resource_type: 'image'
+        })
+        
         //store the image generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:`removed ${object} from the image`,
-            content:imageUrl,
-            type:'image'
-        }); 
+            user_id: userId,
+            prompt: `removed ${object} from the image`,
+            content: imageUrl,
+            type: 'image'
+        });
 
 
-        res.json({success:true,content:imageUrl});
+        res.json({ success: true, content: imageUrl });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -257,15 +259,15 @@ const reviewResume = async (req, res) => {
             return res.json({ success: false, message: "This feature is only avaialble for premium subscriptions" });
         }
 
-        if(resume.size>5*1024*1024){
-            return res.json({success:false,message:"Make sure that resume size is less than 5MB"});
+        if (resume.size > 5 * 1024 * 1024) {
+            return res.json({ success: false, message: "Make sure that resume size is less than 5MB" });
         }
-         
-        const dataBuffer=fs.readFileSync(resume.path);
-        const pdfdata=await pdf(dataBuffer);
 
-        const prompt=`Review the following resume and provide constructive feedback on its strengths,weaknesses an areas for improvement.Resume content:\n\n${pdfdata.text}`
-        
+        const dataBuffer = fs.readFileSync(resume.path);
+        const pdfdata = await pdf(dataBuffer);
+
+        const prompt = `Review the following resume and provide constructive feedback on its strengths,weaknesses an areas for improvement.Resume content:\n\n${pdfdata.text}`
+
         //pass this prompt to gemini api
 
         const response = await ai.chat.completions.create({
@@ -273,33 +275,33 @@ const reviewResume = async (req, res) => {
             messages: [
                 {
                     role: "user",
-                    content:prompt,
+                    content: prompt,
                 },
             ],
-            temperature:0.7,
-            max_tokens:1000
+            temperature: 0.7,
+            max_tokens: 1000
         });
 
 
-        const content=response.choices[0].message.content;
+        const content = response.choices[0].message.content;
 
 
 
         //store the image generated in database
 
         await Creation.create({
-            user_id:userId,
-            prompt:'review the uploaded resume',
-            content:content,
-            type:'resume-review'
-        }); 
+            user_id: userId,
+            prompt: 'review the uploaded resume',
+            content: content,
+            type: 'resume-review'
+        });
 
 
-        res.json({success:true,content});
+        res.json({ success: true, content });
 
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
 }
 
-module.exports={generateArticle,generateBlogTitle,generateImage,removeBackground,removeObject,reviewResume};
+module.exports = { generateArticle, generateBlogTitle, generateImage, removeBackground, removeObject, reviewResume };
