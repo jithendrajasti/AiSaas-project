3a842b6d27f08d27af1f134a1534ceef80f8ef97
jithendrajasti <jithendrajasti11@gmail.com>
2025-07-26 18:46:54 +0530

    cors isue resolve

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ const app=express();
    }
 })();
 
-app.use(cors());
+app.use(cors({
+    origin:['https://ai-saas-project-ten.vercel.app','http://localhost:5173'],
+    credentials:true
+}));
 
 app.use(express.json());
 
